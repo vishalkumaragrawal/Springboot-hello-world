06057115e2b84b0922ce58c714faaad9b1b72a6a
vishalagrawaljava <vishalkumaragrawal067@gmail.com>
2023-05-05 11:54:55 +0530

    Added Hellow world controller

---
+++ src/main/java/com/springbbot/example/HelloWorldExample/HelloWorldExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springbbot.example.HelloWorldExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbbot/example/HelloWorldExample/HelloWorldExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbbot.example.HelloWorldExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
